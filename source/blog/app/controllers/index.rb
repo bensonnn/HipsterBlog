1e46b17410b7136568344f6a01f53db4df6461b2
Batman <batman@devbootcamp.com>
2014-03-13 11:53:58 -0500

    most of the releases

--- source/blog/app/controllers/index.rb
+++ source/blog/app/controllers/index.rb
@@ -1,4 +1,28 @@
 get '/' do
   # Look in app/views/index.erb
-  erb :index
+  erb :index, locals: { posts: Post.all }
+end
+
+get '/show/:post_id' do 
+  post = Post.where(id: params[:post_id])[0]
+  erb :show_post, locals: { post: post }
+end
+
+get '/posts_by_tag/:tag_id' do
+  tag = Tag.where(id: params[:tag_id])
+  posts = tag[0].posts
+  erb :posts_by_tag, locals: { tag: tag, tagged_posts: posts }
+end
+
+get '/create_post' do
+  erb :create_post
+end
+
+post '/create_post' do
+
+  new_post = Post.create(title: params[:title], body: params[:body])
+  params[:tag].split(', ').each do |tag_name|
+    new_post.tags << Tag.create(name: tag_name)
+  end
+  redirect to "/show/#{new_post.id}"
 end
---
+++ source/blog/app/models/tag.rb
@@ -0,0 +1,6 @@
+class Tag < ActiveRecord::Base
+  has_and_belongs_to_many :posts
+  # Remember to create a migration!
+end
+
+
