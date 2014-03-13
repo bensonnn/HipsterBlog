e81f5421950aef6b8d54b396cdbc338e519e1e8b
Joe <jbwilmoth@gmail.com>
2014-03-13 10:48:20 -0500

    Can create a new post and redirect to new post

--- source/blog/app/controllers/posts_controller.rb
+++ source/blog/app/controllers/posts_controller.rb
@@ -3,6 +3,19 @@ get "/posts" do
   erb :"posts/index"
 end
 
+get "/posts/new" do
+  erb :"posts/new"
+end
+
+post "/posts" do
+  post = Post.new(params)
+  if post.save
+    redirect to "/posts/#{post.id}"
+  else
+    redirect to "/posts/new"
+  end
+end
+
 get "/posts/:id" do
   @post = Post.find(params[:id])
   erb :"posts/show"
