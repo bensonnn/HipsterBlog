e188ca2c3b3afd0cbb79f9d70215f85c7b8cbdbc
Apprentice <apprentice@dbc-chicago-018-27.local>
2014-03-16 14:32:19 -0500

    adds user_id to posts and seeds users randomly

--- source/blog/db/seeds.rb
+++ source/blog/db/seeds.rb
@@ -1,5 +1,7 @@
 Tag.destroy_all
 Post.destroy_all
+Word.destroy_all
+User.destroy_all
 
 posts = Array.new(5) { Post.create(title: Hipster.sentence.capitalize, body: Hipster.paragraph) }
 
@@ -16,5 +18,11 @@ File.new("db/words.txt", "r").each do |file|
   end
 end
 
+User.create(username: 'tonyta') {|user| user.password = '1234'}
+User.create(username: 'dannybenson') {|user| user.password = '4321'}
 
+user_ids = User.all.map(&:id)
+Post.all.each do |post|
+  post.update(user_id: user_ids.sample)
+end
 
