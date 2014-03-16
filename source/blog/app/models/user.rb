94f930a257b035d04a5d59f9cf3c687e4b1a6ca6
Apprentice <apprentice@dbc-chicago-018-27.local>
2014-03-16 13:29:41 -0500

    adds migration and model for User

---
+++ source/blog/app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_many :posts
+  attr_accessor :password
+  @password = nil
+
+  before_create :salt_and_hash
+
+  private
+  def salt_and_hash
+    self.salt = PasswordHasher.new_salt
+    self.hashword = PasswordHasher.get_hash( @password , self.salt )
+    @password = nil
+  end
+end
+
