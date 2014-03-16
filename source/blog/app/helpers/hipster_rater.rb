923aab36e7a7d7191c6f63765fa02fce73babde3
Apprentice <dan.s.benson@gmail.com>
2014-03-16 14:34:29 -0500

    HipsterRater extended and tested

--- source/blog/app/helpers/hipster_rater.rb
+++ source/blog/app/helpers/hipster_rater.rb
@@ -10,19 +10,26 @@ module HipsterRater
   class Post
 
     def self.hipness(text)
+      case self.word_count(text)
+      when 1 then "Corny"
+      when 2 then "Rightous"
+      when 3 then "Yeah, man"
+      when 4 then "Solid"
+      else "Too much"
+      end
     end
 
     def self.hip?(text, count=5)
-      hipster_word_count = text.split(" ").count do |word|
-                              Word.where("word = ?", word)
-                            end
-      hipster_word_count >= count
+      self.word_count(text) >= count
     end
 
+    private
+
+    def self.word_count(text)
+      text.split(" ").count{ |word| Word.where("word = ?", word).any? }
+    end
 
   end
-
-
 end
 
 # hip_text = "Synth drinking vinegar next level  post-ironic messenger bag master cleanse keytar pug  Stumptown mustache chillwave Bushwick wolf  PBR beard deep v fingerstache tofu skateboard  Fixie cornhole freegan  Wes Anderson yr actually Cosby sweater PBR shabby chic stumptown  Meh plaid pop-up"
