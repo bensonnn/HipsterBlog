2aae39bc72fed8547a909cc0f44e93f7ddb8a927
Apprentice <apprentice@dbc-chicago-018-27.local>
2014-03-16 14:21:57 -0500

    adds routes for login and new user signup in main controller

--- source/blog/app/controllers/index.rb
+++ source/blog/app/controllers/index.rb
@@ -1,3 +1,38 @@
+enable :session
+
 get '/' do
-  redirect to "/posts"
+  erb :index
+end
+
+post '/' do
+  username = params[:login][:username].downcase
+  password = params[:login][:password]
+  if user = User.find_by_username(username)
+    if user.authenticate(password)
+      session[:username] = username
+      redirect '/posts'
+    else
+      'your password was incorrect'
+    end
+  else
+    "#{username} was not found"
+  end
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  username = params[:login][:username].downcase
+  password = params[:login][:password]
+  if User.find_by_username(username)
+    'that username is already taken!'
+  else
+    User.create(username: username) do |user|
+      user.password = password
+    end
+    session[:username] = username
+    redirect to "/posts"
+  end
 end
